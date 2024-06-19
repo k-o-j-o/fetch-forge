304b8dc17b5be3e20343ae8c4fa0189ba6129cdd
Kaleb Baldwin <baldwinjkaleb@gmail.com>
2024-06-18 20:38:16 -0400

    handle mix of object/FormData on body across multiple configs

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,117 +1,117 @@
-import { isFunction, isIterable, returnThis } from "@/util";
+import { appendEntries, convertToFormData, isFunction, isIterable, returnThis } from "@/util";
 import { HttpConfig, HttpConfigNormalized, HttpConfigSource, HttpContext } from "@/http/http";
 import * as Symbols from "@/symbols";
 
 export function request<Args>(configs: HttpConfigNormalized<Args>[], args: Args) {
-  const context = getDefaultContext();
+	const context = getDefaultContext();
 
-  applyAllConfigs(configs, args, context);
+	applyAllConfigs(configs, args, context);
 
-  const request = new Request(context.url, {
-    method: context.method,
-    headers: context.headers,
-    body: context.body instanceof FormData ? context.body : JSON.stringify(context.body),
-  });
+	const request = new Request(context.url, {
+		method: context.method,
+		headers: context.headers,
+		body: context.body instanceof FormData ? context.body : JSON.stringify(context.body),
+	});
 
-  return fetch(request);
+	return fetch(request);
 }
 
 const CONTEXT_DEFAULTS = {
-  url: new URL(location.origin),
-  method: "get",
-  body: {},
+	url: new URL(location.origin),
+	method: "get",
+	body: {},
 } as const;
 
 function getDefaultContext(): HttpContext {
-  return Object.assign({ headers: new Headers(), params: new URLSearchParams() }, CONTEXT_DEFAULTS);
+	return Object.assign(
+		{ headers: new Headers(), params: new URLSearchParams() },
+		CONTEXT_DEFAULTS
+	);
 }
 
 function applyAllConfigs(
-  configs: HttpConfigSource<any>,
-  args: any,
-  target: HttpContext
+	configs: HttpConfigSource<any>,
+	args: any,
+	target: HttpContext
 ): HttpContext {
-  for (const configOrSource of configs) {
-    if (isIterable(configOrSource)) {
-      applyAllConfigs(configOrSource, args, target);
-    } else {
-      applyConfig(configOrSource, args, target);
-    }
-  }
-  return target;
+	for (const configOrSource of configs) {
+		if (isIterable(configOrSource)) {
+			applyAllConfigs(configOrSource, args, target);
+		} else {
+			applyConfig(configOrSource, args, target);
+		}
+	}
+	return target;
 }
 
 function applyConfig(it: HttpConfig<any>, args: any, context: HttpContext) {
-  const config = normalizeConfig(it);
+	const config = normalizeConfig(it);
 
-  context.method = config.method ?? context.method;
-  applyUrlConfig(config, args, context);
-  applyHeadersConfig(config, args, context);
-  applyParamsConfig(config, args, context);
-  applyBodyConfig(config, args, context);
+	context.method = config.method ?? context.method;
+	applyUrlConfig(config, args, context);
+	applyHeadersConfig(config, args, context);
+	applyParamsConfig(config, args, context);
+	applyBodyConfig(config, args, context);
 }
 
 function normalizeConfig<Args>(config: HttpConfig<Args>): HttpConfigNormalized<Args> {
-  return (
-    config[Symbols.NormalizedConfig] ||
-    (config[Symbols.NormalizedConfig] = {
-      method: config.method,
-      url: isFunction(config.url) ? config.url : returnThis.bind(config.url),
-      headers: isFunction(config.headers) ? config.headers : returnThis.bind(config.headers),
-      params: isFunction(config.params) ? config.params : returnThis.bind(config.params),
-      body: isFunction(config.body) ? config.body : returnThis.bind(config.body),
-    })
-  );
+	return (
+		config[Symbols.NormalizedConfig] ||
+		(config[Symbols.NormalizedConfig] = {
+			method: config.method,
+			url: isFunction(config.url) ? config.url : returnThis.bind(config.url),
+			headers: isFunction(config.headers) ? config.headers : returnThis.bind(config.headers),
+			params: isFunction(config.params) ? config.params : returnThis.bind(config.params),
+			body: isFunction(config.body) ? config.body : returnThis.bind(config.body),
+		})
+	);
 }
 
 const beginsWithSlash = /^[\\\/]/;
 const beginsWithProtocol = /^http/;
 
 function applyUrlConfig(config: HttpConfigNormalized<any>, args: any, target: HttpContext) {
-  const url = config.url(args);
-  if (url instanceof URL) {
-    target.url = url;
-  } else if (beginsWithSlash.test(url) || beginsWithProtocol.test(url)) {
-    target.url = new URL(url);
-  } else {
-    target.url.pathname += url;
-  }
+	const url = config.url(args);
+	if (url instanceof URL) {
+		//a URL instance must represent an absolute path, so we should replace the whole thing
+		target.url = url;
+	} else if (beginsWithSlash.test(url) || beginsWithProtocol.test(url)) {
+		target.url = new URL(url);
+	} else {
+		target.url.pathname += url;
+	}
 }
 
 function applyHeadersConfig(config: HttpConfigNormalized<any>, args: any, target: HttpContext) {
-  const headers = config.headers(args);
-  if (headers instanceof Headers) {
-    appendEntries(target.headers, headers);
-  } else {
-    appendEntries(target.headers, Object.entries(headers));
-  }
+	const headers = config.headers(args);
+	if (headers instanceof Headers) {
+		appendEntries(target.headers, headers);
+	} else {
+		appendEntries(target.headers, Object.entries(headers));
+	}
 }
 
 function applyParamsConfig(config: HttpConfigNormalized<any>, args: any, target: HttpContext) {
-  const params = config.params(args);
-  if (params instanceof URLSearchParams) {
-    appendEntries(target.params, params);
-  } else {
-    appendEntries(target.params, Object.entries(params));
-  }
+	const params = config.params(args);
+	if (params instanceof URLSearchParams) {
+		appendEntries(target.params, params);
+	} else {
+		appendEntries(target.params, Object.entries(params));
+	}
 }
 
 function applyBodyConfig(config: HttpConfigNormalized<any>, args: any, target: HttpContext) {
-  const body = config.body(args);
-  if (body instanceof FormData) {
-    if (!(target.body instanceof FormData)) {
-      const formData = new FormData();
-      appendEntries(formData, Object.entries(target.body));
-      target.body = formData;
-    }
-    appendEntries(target.body, body);
-  } else {
-    Object.assign(target.body, body);
-  }
-}
-
-function appendEntries(appendable: Appendable, entries: Iterable<KeyValuePair<string, unknown>>) {
-  for (const [key, value] of entries) {
-    appendable.append(key, value);
-  }
+	let body = config.body(args);
+	if (body instanceof FormData) {
+		if (!(target.body instanceof FormData)) {
+			target.body = convertToFormData(target.body);
+		}
+		appendEntries(target.body, body);
+	} else {
+		if (target.body instanceof FormData) {
+			body = convertToFormData(body);
+			appendEntries(target.body, body);
+		}
+		Object.assign(target.body, body);
+	}
 }
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,15 +1,30 @@
 export function isFunction(thing: any): thing is Function {
-  return typeof thing === "function";
+	return typeof thing === "function";
 }
 
 export function isIterable(thing: any): thing is Iterable<any> {
-  return thing && thing[Symbol.iterator];
+	return thing && thing[Symbol.iterator];
 }
 
 export function isObject(thing: any): thing is object {
-  return thing && typeof thing === "object";
+	return thing && typeof thing === "object";
 }
 
 export function returnThis<T>(this: T): T {
-  return this;
+	return this;
+}
+
+export function appendEntries(
+	appendable: Appendable,
+	entries: Iterable<KeyValuePair<string, unknown>>
+) {
+	for (const [key, value] of entries) {
+		appendable.append(key, value);
+	}
+}
+
+export function convertToFormData(obj: object) {
+	const formData = new FormData();
+	appendEntries(formData, Object.entries(obj));
+	return formData;
 }
